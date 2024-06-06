857091c2b4a05d0a9bec88e701285d1c1dbaeb00
faoudia <fares.aoudia0@gmail.com>
2024-06-06 14:55:36 +0200

    Main file

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	src "ascii-art/src"
+	"fmt"
 )
 
 func main() {
 	content := src.FileOpen("banners/shadow.txt")
-	fmt.Println(src.GetLetter(content, 97))
+	fmt.Println(src.GetLetter(content, 98))
 }
