7c177612aed55e93fb9f8444d2d0c590877514b2
Emixam26 <maxime.trebert@gmail.com>
2024-06-06 16:37:22 +0200

    get input

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import (
 	src "ascii-art/src"
 	"fmt"
+	"os"
 )
 
 func main() {
-	content := src.FileOpen("banners/shadow.txt")
-	test := "hello"
 
-	for _, char := range test {
-		fmt.Println(src.GetLetter(content, int(char)))
+	// Checking if the correct number of arguments is provided
+	if len(os.Args) < 2 {
+		fmt.Println("Wrong use of the tool")
+		return
+	}
+	// Get the input string from command line arguments
+	input := os.Args[1:]
+	content := src.FileOpen("banners/shadow.txt")
+
+	for _, word := range input {
+		if word == "\n" {
+			fmt.Println("\n")
+			continue
+		}
+		for _, char := range word {
+			fmt.Println(src.GetLetter(content, int(char)))
+		}
+		//Add a space after each word
+		fmt.Println(src.GetLetter(content, 32))
 	}
 }
