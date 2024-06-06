99a1ace645cd260561522ea47396916bea2ddac1
faoudia <fares.aoudia0@gmail.com>
2024-06-06 14:54:24 +0200

    Get letter from file

--- src/getletter.go
+++ src/getletter.go
@@ -1,5 +1,26 @@
 package src
 
+import (
+	"strings"
+)
+
+const LETTER_HEIGHT = 8
+
 func GetLetter(content string, ascii int) string {
-	
+	str := ""
+	lines := strings.Split(content, "\n")
+
+	place := ascii - 31
+	times := (place - 1) * LETTER_HEIGHT
+	beginning := (ascii - 30) + times
+
+	for i := beginning; i < beginning+LETTER_HEIGHT; i++ {
+		if i != (beginning+LETTER_HEIGHT)-1 {
+			str += lines[i-1] + "\n"
+		} else {
+			str += lines[i-1]
+		}
+	}
+
+	return str
 }
