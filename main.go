755365d3a48202a82f409b94a1f74005d68fea1b
Emixam26 <maxime.trebert@gmail.com>
2024-06-06 18:30:33 +0200

    input loader

--- main.go
+++ main.go
@@ -11,23 +11,33 @@ func main() {
 
 	// Checking if the correct number of arguments is provided
 	if len(os.Args) < 2 {
-		fmt.Println("Wrong use of the tool")
+		fmt.Println("Usage: go run . <string>")
 		return
 	}
+
 	// Get the input string from command line arguments
 	input := os.Args[1]
-	content := src.FileOpen("banners/standard.txt")
 
-	lines := make([]string, 8)
+	// Load the standard banner file
+	bannerFile := "banners/standard.txt"
+	content := src.FileOpen(bannerFile)
 
-	for _, char := range input {
-		c := strings.Split(src.GetLetter(content, int(char)), "\n")
-		for i, l := range c {
-			lines[i] += l
+	// Split the input by newline character
+	segments := strings.Split(input, "\\n")
+	finalOutput := make([]string, 0)
+
+	for _, segment := range segments {
+		lines := make([]string, 8)
+		for _, char := range segment {
+			c := strings.Split(src.GetLetter(content, int(char)), "\n")
+			for i, l := range c {
+				lines[i] += l
+			}
 		}
+		finalOutput = append(finalOutput, lines...)
 	}
-	for _, line := range lines {
+
+	for _, line := range finalOutput {
 		fmt.Println(line)
 	}
-
 }
