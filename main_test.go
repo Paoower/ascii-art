d9fe9f5f82308eec7b124f5d8f08fa1ebd4a2925
Farès Aoudia <fares.aoudia0@gmail.com>
2024-06-07 13:25:08 +0200

    Unit Test

--- main_test.go
+++ main_test.go
@@ -7,8 +7,11 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	correct := []string{" _    _          _   _          ", "| |  | |        | | | |         ", "| |__| |   ___  | | | |   ___   ", "|  __  |  / _ \\ | | | |  / _ \\  ", "| |  | | |  __/ | | | | | (_) | ", "|_|  |_|  \\___| |_| |_|  \\___/  ", "                                ", "                                "}
+	correct := ` _    _          _   _          | |  | |        | | | |         | |__| |   ___  | | | |   ___   |  __  |  / _ \ | | | |  / _ \  | |  | | |  __/ | | | | | (_) | |_|  |_|  \___| |_| |_|  \___/                                                                  `
+
 	input := "Hello"
+	output := ""
+
 	lines := make([]string, 0)
 	words := strings.Split(input, "\\n")
 
@@ -16,17 +19,22 @@ func TestHello(t *testing.T) {
 		lines = append(lines, src.GetWord(word, "banners/standard.txt")...)
 	}
 
-	for i, line := range lines {
-		if correct[i] != line {
-			t.Errorf("Program Output: %v \n", lines)
-			t.Errorf("Correct Output: %v \n", correct)
-		}
+	for _, line := range lines {
+		output += line
+	}
+
+	if output != correct {
+		t.Errorf("Program Output: %v \n", output)
+		t.Errorf("Correct Output: %v \n", correct)
 	}
 }
 
-func TestHelloThere(t *testing.T) {
-	correct := []string{" _    _          _   _               _______   _                           ", "| |  | |        | | | |             |__   __| | |                          ", "| |__| |   ___  | | | |   ___          | |    | |__     ___   _ __    ___  ", "|  __  |  / _ \\ | | | |  / _ \\         | |    |  _ \\   / _ \\ | '__|  / _ \\ ", "| |  | | |  __/ | | | | | (_) |        | |    | | | | |  __/ | |    |  __/ ", "|_|  |_|  \\___| |_| |_|  \\___/         |_|    |_| |_|  \\___| |_|     \\___| ", "                                                                           ", "                                                                           "}
-	input := "Hello There"
+func TestHELLO(t *testing.T) {
+	correct := ` _    _   ______   _        _         ____   | |  | | |  ____| | |      | |       / __ \  | |__| | | |__    | |      | |      | |  | | |  __  | |  __|   | |      | |      | |  | | | |  | | | |____  | |____  | |____  | |__| | |_|  |_| |______| |______| |______|  \____/                                                                                            `
+
+	input := "HELLO"
+	output := ""
+
 	lines := make([]string, 0)
 	words := strings.Split(input, "\\n")
 
@@ -34,10 +42,58 @@ func TestHelloThere(t *testing.T) {
 		lines = append(lines, src.GetWord(word, "banners/standard.txt")...)
 	}
 
-	for i, line := range lines {
-		if correct[i] != line {
-			t.Errorf("Program Output: %v \n", lines)
-			t.Errorf("Correct Output: %v \n", correct)
-		}
+	for _, line := range lines {
+		output += line
+	}
+
+	if output != correct {
+		t.Errorf("Program Output: %v \n", output)
+		t.Errorf("Correct Output: %v \n", correct)
+	}
+}
+
+func TestHeLloHuMaN(t *testing.T) {
+	correct := " _    _          _        _               _    _           __  __           _   _  | |  | |        | |      | |             | |  | |         |  \\/  |         | \\ | | | |__| |   ___  | |      | |   ___       | |__| |  _   _  | \\  / |   __ _  |  \\| | |  __  |  / _ \\ | |      | |  / _ \\      |  __  | | | | | | |\\/| |  / _` | | . ` | | |  | | |  __/ | |____  | | | (_) |     | |  | | | |_| | | |  | | | (_| | | |\\  | |_|  |_|  \\___| |______| |_|  \\___/      |_|  |_|  \\__,_| |_|  |_|  \\__,_| |_| \\_|                                                                                                                                                                       "
+
+	input := "HeLlo HuMaN"
+	output := ""
+
+	lines := make([]string, 0)
+	words := strings.Split(input, "\\n")
+
+	for _, word := range words {
+		lines = append(lines, src.GetWord(word, "banners/standard.txt")...)
+	}
+
+	for _, line := range lines {
+		output += line
+	}
+
+	if output != correct {
+		t.Errorf("Program Output: %v \n", output)
+		t.Errorf("Correct Output: %v \n", correct)
+	}
+}
+
+func Test1Hello2There(t *testing.T) {
+	correct := "     _    _          _   _                       _______   _                            _  | |  | |        | | | |              ____   |__   __| | |                          / | | |__| |   ___  | | | |   ___       |___ \\     | |    | |__     ___   _ __    ___  | | |  __  |  / _ \\ | | | |  / _ \\        __) |    | |    |  _ \\   / _ \\ | '__|  / _ \\ | | | |  | | |  __/ | | | | | (_) |      / __/     | |    | | | | |  __/ | |    |  __/ |_| |_|  |_|  \\___| |_| |_|  \\___/      |_____|    |_|    |_| |_|  \\___| |_|     \\___|                                                                                                                                                                               "
+
+	input := "1Hello 2There"
+	output := ""
+
+	lines := make([]string, 0)
+	words := strings.Split(input, "\\n")
+
+	for _, word := range words {
+		lines = append(lines, src.GetWord(word, "banners/standard.txt")...)
+	}
+
+	for _, line := range lines {
+		output += line
+	}
+
+	if output != correct {
+		t.Errorf("Program Output: %v \n", output)
+		t.Errorf("Correct Output: %v \n", correct)
 	}
 }
