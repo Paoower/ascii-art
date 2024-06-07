17fba047a6dfc0afbd4ff628fde8c0c6ef4a9574
Emixam26 <maxime.trebert@gmail.com>
2024-06-07 15:43:04 +0200

    test cat -e

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ INPUTS=(
     "HELLO"
     "HeLlo HuMaN"
     "1Hello 2There"
-    "Hello\nThere"
+    "Hello\n\nThere"
     "{Hello & There #}"
     "hello There 1 to 2!"
     "MaD3IrA&LiSboN"
@@ -29,10 +29,8 @@ printf "\n \033[36m%s\033[0m" "////////////////////////TESTS////////////////////
 printf "\n \033[35m◼\033[0m : Input \033[32m◼\033[0m : Output \n"
 
 for input in "${INPUTS[@]}"; do
-  printf "\033[35m \n Test for go run . %s : \n" "\"$input"\" 
-  output="$($GO_PROGRAM "$input")"
+  printf "\033[35m \n Test for go run . %s | cat -e : \n" "\"$input"\" 
+  output="$($GO_PROGRAM "$input" | cat -e)" 
   printf "\033[32m%s\n" "$output"
   printf "\033[36m%s\033[0m\n" "//////////////////////////////////////////////////////////"
 done
-
-
